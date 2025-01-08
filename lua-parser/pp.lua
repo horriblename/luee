f4d721ceb5c9c8650dcc62a02d0f9e9132e1961d
Ching Pei Yang <badnam3o.0@gmail.com>
2025-01-08 06:53:30 +0100

    pp: process fn

--- lua-parser/pp.lua
+++ lua-parser/pp.lua
@@ -6,28 +6,37 @@ local pp = {}
 local block2str, stm2str, exp2str, var2str
 local explist2str, varlist2str, parlist2str, fieldlist2str
 
-local function iscntrl (x)
+local function iscntrl(x)
   if (x >= 0 and x <= 31) or (x == 127) then return true end
   return false
 end
 
-local function isprint (x)
+local function isprint(x)
   return not iscntrl(x)
 end
 
-local function fixed_string (str)
+local function fixed_string(str)
   local new_str = ""
-  for i=1,string.len(str) do
+  for i = 1, string.len(str) do
     char = string.byte(str, i)
-    if char == 34 then new_str = new_str .. string.format("\\\"")
-    elseif char == 92 then new_str = new_str .. string.format("\\\\")
-    elseif char == 7 then new_str = new_str .. string.format("\\a")
-    elseif char == 8 then new_str = new_str .. string.format("\\b")
-    elseif char == 12 then new_str = new_str .. string.format("\\f")
-    elseif char == 10 then new_str = new_str .. string.format("\\n")
-    elseif char == 13 then new_str = new_str .. string.format("\\r")
-    elseif char == 9 then new_str = new_str .. string.format("\\t")
-    elseif char == 11 then new_str = new_str .. string.format("\\v")
+    if char == 34 then
+      new_str = new_str .. string.format("\\\"")
+    elseif char == 92 then
+      new_str = new_str .. string.format("\\\\")
+    elseif char == 7 then
+      new_str = new_str .. string.format("\\a")
+    elseif char == 8 then
+      new_str = new_str .. string.format("\\b")
+    elseif char == 12 then
+      new_str = new_str .. string.format("\\f")
+    elseif char == 10 then
+      new_str = new_str .. string.format("\\n")
+    elseif char == 13 then
+      new_str = new_str .. string.format("\\r")
+    elseif char == 9 then
+      new_str = new_str .. string.format("\\t")
+    elseif char == 11 then
+      new_str = new_str .. string.format("\\v")
     else
       if isprint(char) then
         new_str = new_str .. string.format("%c", char)
@@ -39,26 +48,26 @@ local function fixed_string (str)
   return new_str
 end
 
-local function name2str (name)
+local function name2str(name)
   return string.format('"%s"', name)
 end
 
-local function boolean2str (b)
+local function boolean2str(b)
   return string.format('"%s"', tostring(b))
 end
 
-local function number2str (n)
+local function number2str(n)
   return string.format('"%s"', tostring(n))
 end
 
-local function string2str (s)
+local function string2str(s)
   return string.format('"%s"', fixed_string(s))
 end
 
-function var2str (var)
+function var2str(var)
   local tag = var.tag
   local str = "`" .. tag
-  if tag == "Id" then -- `Id{ <string> }
+  if tag == "Id" then        -- `Id{ <string> }
     str = str .. " " .. name2str(var[1])
   elseif tag == "Index" then -- `Index{ expr expr }
     str = str .. "{ "
@@ -71,7 +80,7 @@ function var2str (var)
   return str
 end
 
-function varlist2str (varlist)
+function varlist2str(varlist)
   local l = {}
   for k, v in ipairs(varlist) do
     l[k] = var2str(v)
@@ -79,7 +88,7 @@ function varlist2str (varlist)
   return "{ " .. table.concat(l, ", ") .. " }"
 end
 
-function parlist2str (parlist)
+function parlist2str(parlist)
   local l = {}
   local len = #parlist
   local is_vararg = false
@@ -98,7 +107,7 @@ function parlist2str (parlist)
   return "{ " .. table.concat(l, ", ") .. " }"
 end
 
-function fieldlist2str (fieldlist)
+function fieldlist2str(fieldlist)
   local l = {}
   for k, v in ipairs(fieldlist) do
     local tag = v.tag
@@ -117,25 +126,31 @@ function fieldlist2str (fieldlist)
   end
 end
 
-function exp2str (exp)
+function exp2str(exp)
   local tag = exp.tag
   local str = "`" .. tag
   if tag == "Nil" or
-     tag == "Dots" then
-  elseif tag == "Boolean" then -- `Boolean{ <boolean> }
+      tag == "Dots" then
+  elseif tag == "Boolean" then  -- `Boolean{ <boolean> }
     str = str .. " " .. boolean2str(exp[1])
-  elseif tag == "Number" then -- `Number{ <number> }
+  elseif tag == "Number" then   -- `Number{ <number> }
     str = str .. " " .. number2str(exp[1])
-  elseif tag == "String" then -- `String{ <string> }
+  elseif tag == "String" then   -- `String{ <string> }
     str = str .. " " .. string2str(exp[1])
   elseif tag == "Function" then -- `Function{ { `Id{ <string> }* `Dots? } block }
     str = str .. "{ "
     str = str .. parlist2str(exp[1]) .. ", "
     str = str .. block2str(exp[2])
     str = str .. " }"
+  elseif tag == "Fn" then
+    str = "Fn"
+    str = str .. "{ "
+    str = str .. parlist2str(exp[1]) .. ", "
+    str = str .. exp2str(exp[2])
+    str = str .. " }"
   elseif tag == "Table" then -- `Table{ ( `Pair{ expr expr } | expr )* }
     str = str .. fieldlist2str(exp)
-  elseif tag == "Op" then -- `Op{ opid expr expr? }
+  elseif tag == "Op" then    -- `Op{ opid expr expr? }
     str = str .. "{ "
     str = str .. name2str(exp[1]) .. ", "
     str = str .. exp2str(exp[2])
@@ -145,11 +160,11 @@ function exp2str (exp)
     str = str .. " }"
   elseif tag == "Paren" then -- `Paren{ expr }
     str = str .. "{ " .. exp2str(exp[1]) .. " }"
-  elseif tag == "Call" then -- `Call{ expr expr* }
+  elseif tag == "Call" then  -- `Call{ expr expr* }
     str = str .. "{ "
     str = str .. exp2str(exp[1])
     if exp[2] then
-      for i=2, #exp do
+      for i = 2, #exp do
         str = str .. ", " .. exp2str(exp[i])
       end
     end
@@ -159,13 +174,13 @@ function exp2str (exp)
     str = str .. exp2str(exp[1]) .. ", "
     str = str .. exp2str(exp[2])
     if exp[3] then
-      for i=3, #exp do
+      for i = 3, #exp do
         str = str .. ", " .. exp2str(exp[i])
       end
     end
     str = str .. " }"
-  elseif tag == "Id" or -- `Id{ <string> }
-         tag == "Index" then -- `Index{ expr expr }
+  elseif tag == "Id" or      -- `Id{ <string> }
+      tag == "Index" then    -- `Index{ expr expr }
     str = var2str(exp)
   else
     error("expecting an expression, but got a " .. tag)
@@ -173,7 +188,7 @@ function exp2str (exp)
   return str
 end
 
-function explist2str (explist)
+function explist2str(explist)
   local l = {}
   for k, v in ipairs(explist) do
     l[k] = exp2str(v)
@@ -185,7 +200,7 @@ function explist2str (explist)
   end
 end
 
-function stm2str (stm)
+function stm2str(stm)
   local tag = stm.tag
   local str = "`" .. tag
   if tag == "Do" then -- `Do{ stat* }
@@ -214,16 +229,16 @@ function stm2str (stm)
     local len = #stm
     if len % 2 == 0 then
       local l = {}
-      for i=1,len-2,2 do
-        str = str .. exp2str(stm[i]) .. ", " .. block2str(stm[i+1]) .. ", "
+      for i = 1, len - 2, 2 do
+        str = str .. exp2str(stm[i]) .. ", " .. block2str(stm[i + 1]) .. ", "
       end
-      str = str .. exp2str(stm[len-1]) .. ", " .. block2str(stm[len])
+      str = str .. exp2str(stm[len - 1]) .. ", " .. block2str(stm[len])
     else
       local l = {}
-      for i=1,len-3,2 do
-        str = str .. exp2str(stm[i]) .. ", " .. block2str(stm[i+1]) .. ", "
+      for i = 1, len - 3, 2 do
+        str = str .. exp2str(stm[i]) .. ", " .. block2str(stm[i + 1]) .. ", "
       end
-      str = str .. exp2str(stm[len-2]) .. ", " .. block2str(stm[len-1]) .. ", "
+      str = str .. exp2str(stm[len - 2]) .. ", " .. block2str(stm[len - 1]) .. ", "
       str = str .. block2str(stm[len])
     end
     str = str .. " }"
@@ -259,8 +274,8 @@ function stm2str (stm)
     str = str .. "{ " .. var2str(stm[1][1]) .. " }, "
     str = str .. "{ " .. exp2str(stm[2][1]) .. " }"
     str = str .. " }"
-  elseif tag == "Goto" or -- `Goto{ <string> }
-         tag == "Label" then -- `Label{ <string> }
+  elseif tag == "Goto" or     -- `Goto{ <string> }
+      tag == "Label" then     -- `Label{ <string> }
     str = str .. "{ " .. name2str(stm[1]) .. " }"
   elseif tag == "Return" then -- `Return{ <expr>* }
     str = str .. explist2str(stm)
@@ -269,7 +284,7 @@ function stm2str (stm)
     str = str .. "{ "
     str = str .. exp2str(stm[1])
     if stm[2] then
-      for i=2, #stm do
+      for i = 2, #stm do
         str = str .. ", " .. exp2str(stm[i])
       end
     end
@@ -279,7 +294,7 @@ function stm2str (stm)
     str = str .. exp2str(stm[1]) .. ", "
     str = str .. exp2str(stm[2])
     if stm[3] then
-      for i=3, #stm do
+      for i = 3, #stm do
         str = str .. ", " .. exp2str(stm[i])
       end
     end
@@ -290,7 +305,7 @@ function stm2str (stm)
   return str
 end
 
-function block2str (block)
+function block2str(block)
   local l = {}
   for k, v in ipairs(block) do
     l[k] = stm2str(v)
@@ -298,25 +313,25 @@ function block2str (block)
   return "{ " .. table.concat(l, ", ") .. " }"
 end
 
-function pp.tostring (t)
+function pp.tostring(t)
   assert(type(t) == "table")
   return block2str(t)
 end
 
-function pp.print (t)
+function pp.print(t)
   assert(type(t) == "table")
   print(pp.tostring(t))
 end
 
-function pp.dump (t, i)
+function pp.dump(t, i)
   if i == nil then i = 0 end
   io.write(string.format("{\n"))
-  io.write(string.format("%s[tag] = %s\n", string.rep(" ", i+2), t.tag or "nil"))
-  io.write(string.format("%s[pos] = %s\n", string.rep(" ", i+2), t.pos or "nil"))
-  for k,v in ipairs(t) do
-    io.write(string.format("%s[%s] = ", string.rep(" ", i+2), tostring(k)))
+  io.write(string.format("%s[tag] = %s\n", string.rep(" ", i + 2), t.tag or "nil"))
+  io.write(string.format("%s[pos] = %s\n", string.rep(" ", i + 2), t.pos or "nil"))
+  for k, v in ipairs(t) do
+    io.write(string.format("%s[%s] = ", string.rep(" ", i + 2), tostring(k)))
     if type(v) == "table" then
-      pp.dump(v,i+2)
+      pp.dump(v, i + 2)
     else
       io.write(string.format("%s\n", tostring(v)))
     end
